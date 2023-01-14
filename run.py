96948ffcfab1a647446626c38acf8d79fc23c87b
Folarin Ogungbemi <Folarine1@gmail.com>
2023-01-14 02:05:26 +0000

    set up environment and connect db

---
+++ run.py
@@ -0,0 +1,10 @@
+import os
+from taskmanager import app
+
+# running th app
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=os.environ.get("DEBUG"),
+    )
